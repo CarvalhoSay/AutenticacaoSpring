a13b358f74c64f62ad59cd0311a75244e043544f
CarvalhoSay <saymoncarvalho@terra.com.br>
2025-10-21 22:01:22 -0300

    Criacao da SecurityFilter, UserDetails e SecurityConfig

--- application/src/main/java/com/authspring/application/domain/repositories/UserRepository.java
+++ application/src/main/java/com/authspring/application/domain/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.authspring.application.domain.repositories;
 import com.authspring.application.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail (String email);
 }
